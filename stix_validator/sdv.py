c82290991485b2f0926f7cf3a4e639e3ddf8fa17
Bryan Worrell <bworrell@mitre.org>
2013-09-12 13:37:32 -0400

    added STIXValidator class which performs best practice guidance check as well as schema validation

--- stix_validator/sdv.py
+++ stix_validator/sdv.py
@@ -8,7 +8,7 @@ STIX Document Validator (sdv) - validates STIX v1.0 instance documents.
 
 import os
 import argparse
-from validator import XmlValidator 
+from validator import STIXValidator 
 
 def get_files_to_validate(dir):
     '''Return a list of xml files under a directory'''
@@ -26,12 +26,81 @@ def error(msg):
     print "[!] %s" % (msg)
     exit(1)
 
+def print_result(fp, isvalid, validation_error, warnings):
+    if isvalid:
+        print "[+] %s : VALID" % (fp)
+        
+        if warnings:
+            duplicate_ids = warnings.get('duplicate_ids')
+            if duplicate_ids:
+                print '    [~] Nodes with duplicate ids'
+                for id_, list_nodes in duplicate_ids.iteritems():
+                    print '    [~] id: [%s]' % (id_)
+                    for node in list_nodes:
+                        print '       [%s] line: [%s]' % (node.tag, node.sourceline)
+                print
+            
+            missing_ids = warnings.get('missing_ids')
+            if missing_ids:
+                print '    [~] Nodes with missing ids'
+                for node in missing_ids:
+                    print '    [~] [%s] line: [%s]' % (node.tag, node.sourceline)
+                print
+            
+            unresolved_idrefs = warnings.get('unresolved_idrefs')
+            if unresolved_idrefs:
+                print '    [~] Nodes with idrefs that do not resolve'
+                for node in unresolved_idrefs:
+                    print '    [~] [%s] idref: [%s] line: [%s]' % (node.tag, node.attrib.get('idref'), node.sourceline)
+                print
+                
+            formatted_ids = warnings.get('id_format')
+            if formatted_ids:
+                print '    [~] Nodes with ids not formatted as [ns_prefix]:[object-type]-[GUID]'
+                for node in formatted_ids:
+                    print '    [~] [%s] id: [%s] line: [%s]' % (node.tag, node.attrib.get('id'), node.sourceline)
+                print
+            
+            idrefs_with_content = warnings.get('idref_with_content')
+            if idrefs_with_content:
+                print '    [~] Nodes that declare idrefs but also contain content'
+                for node in idrefs_with_content:
+                    print '    [~] [%s] idref: [%s] line: [%s]' % (node.tag, node.attrib.get('idref'), node.sourceline)
+                print
+                
+            indicator_suggestions = warnings.get('indicator_suggestions')
+            if indicator_suggestions:
+                print '    [~] Indicator suggestions'
+                for indicator_dict in indicator_suggestions:
+                    node = indicator_dict['node']
+                    list_missing = []
+                    if not indicator_dict.get('title', True):
+                        list_missing.append('Title')
+                    if not indicator_dict.get('description', True):
+                        list_missing.append('Description')
+                    if not indicator_dict.get('type', True):
+                        list_missing.append('Type')
+                    if not indicator_dict.get('valid_time_position', True):
+                        list_missing.append('Valid_Time_Position')
+                    if not indicator_dict.get('indicated_ttp', True):
+                        list_missing.append('Indicated_TTP')
+                    if not indicator_dict.get('confidence', True):
+                        list_missing.append('Confidence')
+                    
+                    print '    [~] id: [%s] line: [%s] missing: %s' % (indicator_dict.get('id'), node.sourceline, list_missing)
+                    
+    else:
+        print "[!] %s : INVALID : [%s]" % (fp, str(validation_error))
+                    
+
+
 def main():
-    parser = argparse.ArgumentParser(description="STIX Document Validator (sdv) - validated STIX v1.0 instance documents")
+    parser = argparse.ArgumentParser(description="STIX Document Validator")
     parser.add_argument("--schema-dir", dest="schema_dir", default=None, help="Path to directory containing all necessary schemas for validation")
     parser.add_argument("--input-file", dest="infile", default=None, help="Path to STIX instance document to validate")
     parser.add_argument("--input-dir", dest="indir", default=None, help="Path to directory containing STIX instance documents to validate")
     parser.add_argument("--use-schemaloc", dest="use_schemaloc", action='store_true', default=False, help="Use schemaLocation attribute to determine schema locations.")
+    parser.add_argument("--best-practices", dest="best_practices", action='store_true', default=False, help="Check that the document follows authoring best practices")
     
     args = parser.parse_args()
     
@@ -54,14 +123,11 @@ def main():
     
     if len(to_validate) > 0:
         print "[-] Processing %s files" % (len(to_validate))
-        ssv = XmlValidator(schema_dir=args.schema_dir, use_schemaloc=args.use_schemaloc)
+        ssv = STIXValidator(schema_dir=args.schema_dir, use_schemaloc=args.use_schemaloc, best_practices=args.best_practices)
         for fp in to_validate:
             with open(fp, 'rb') as f:
-                (result, msg) = ssv.validate(f)
-                if result:
-                    print "[+] %s : VALID" % (fp)
-                else:
-                    print "[!] %s : INVALID : %s" % (fp, msg)
+                (isvalid, validation_error, best_practice_warnings) = ssv.validate(f)
+                print_result(fp, isvalid, validation_error, best_practice_warnings)
 
 if __name__ == '__main__':
     main()
--- stix_validator/validator.py
+++ stix_validator/validator.py
@@ -2,13 +2,13 @@
 # See LICENSE.txt for complete terms.
 
 import os
+import re
 from collections import defaultdict
 from lxml import etree as et
 
-NS_XML_SCHEMA_INSTANCE = "http://www.w3.org/2001/XMLSchema-instance"
-NS_XML_SCHEMA = "http://www.w3.org/2001/XMLSchema"
-
 class XmlValidator(object):
+    NS_XML_SCHEMA_INSTANCE = "http://www.w3.org/2001/XMLSchema-instance"
+    NS_XML_SCHEMA = "http://www.w3.org/2001/XMLSchema"
     
     def __init__(self, schema_dir=None, use_schemaloc=False):
         self.__imports = self._build_imports(schema_dir)
@@ -40,7 +40,7 @@ class XmlValidator(object):
         list_schemas - a list of schema file paths that all belong to the same namespace
         '''
         parent_schema = None
-        tag_include = "{%s}include" % (NS_XML_SCHEMA)
+        tag_include = "{%s}include" % (self.NS_XML_SCHEMA)
         
         for fn in list_schemas:
             tree = et.parse(fn)
@@ -90,7 +90,7 @@ class XmlValidator(object):
         schema_txt = '''<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" targetNamespace="http://stix.mitre.org/tools/validator" elementFormDefault="qualified" attributeFormDefault="qualified"/>'''
         root = et.fromstring(schema_txt)
         
-        tag_import = "{%s}import" % (NS_XML_SCHEMA)
+        tag_import = "{%s}import" % (self.NS_XML_SCHEMA)
         for ns, list_schemaloc in import_dict.iteritems():
             schemaloc = list_schemaloc
             schemaloc = schemaloc.replace("\\", "/")
@@ -103,7 +103,7 @@ class XmlValidator(object):
     def _extract_schema_locations(self, root):
         schemaloc_dict = {}
         
-        tag_schemaloc = "{%s}schemaLocation" % (NS_XML_SCHEMA_INSTANCE)
+        tag_schemaloc = "{%s}schemaLocation" % (self.NS_XML_SCHEMA_INSTANCE)
         schemaloc = root.attrib[tag_schemaloc].split()
         schemaloc_pairs = zip(schemaloc[::2], schemaloc[1::2])
         
@@ -151,4 +151,161 @@ class XmlValidator(object):
         except Exception as e:
             return (False, str(e))
 
+
+class STIXValidator(XmlValidator):
+    '''Schema validates STIX v1.0 documents and checks best practice guidance'''
+    __stix_version__ = "1.0"
+    
+    PREFIX_STIX_CORE = 'stix'
+    PREFIX_CYBOX_CORE = 'cybox'
+    PREFIX_STIX_INDICATOR = 'indicator'
+    
+    NS_STIX_CORE = "http://stix.mitre.org/stix-1"
+    NS_STIX_INDICATOR = "http://stix.mitre.org/Indicator-2"
+    NS_CYBOX_CORE = "http://cybox.mitre.org/cybox-2"
+    
+    NS_MAP = {PREFIX_CYBOX_CORE : NS_CYBOX_CORE,
+              PREFIX_STIX_CORE : NS_STIX_CORE,
+              PREFIX_STIX_INDICATOR : NS_STIX_INDICATOR}
+    
+    def __init__(self, schema_dir=None, use_schemaloc=False, best_practices=False):
+        super(STIXValidator, self).__init__(schema_dir, use_schemaloc)
+        self.best_practices = best_practices
+        
+    def _check_id_presence_and_format(self, instance_doc):
+        return_dict = {'no_id' : [],
+                       'format' : []}
+        
+        elements_to_check = ['stix:Campaign',
+                             'stix:Course_Of_Action',
+                             'stix:Exploit_Target',
+                             'stix:Incident',
+                             'stix:Indicator',
+                             'stix:STIX_Package',
+                             'stix:Thread_Actor',
+                             'stix:TTP',
+                             'cybox:Observable',
+                             'cybox:Object',
+                             'cybox:Event',
+                             'cybox:Action']
+    
+        for tag in elements_to_check:
+            xpath = ".//%s" % (tag)
+            elements = instance_doc.xpath(xpath, namespaces=self.NS_MAP)
+            
+            for e in elements:
+                try:
+                    if not re.match(r'\w+:\w+-', e.attrib['id']): # not the best regex
+                        return_dict['format'].append(e)
+                except KeyError as ex:
+                    return_dict['no_id'].append(e)
+            
+        return return_dict
+    
+    def _check_duplicate_ids(self, instance_doc):
+        dict_id_nodes = defaultdict(list)
+        dup_dict = {}
+        xpath_all_nodes_with_ids = "//*[@id]"
+        
+        all_nodes_with_ids = instance_doc.xpath(xpath_all_nodes_with_ids)
+        for node in all_nodes_with_ids:
+            dict_id_nodes[node.attrib['id']].append(node)
+        
+        for k,v in dict_id_nodes.iteritems():
+            if len(v) > 1:
+                dup_dict[k] = v
+        
+        return dup_dict
+    
+    def _check_idref_resolution(self, instance_doc):
+        list_unresolved_ids = []
+        xpath_all_idrefs = "//*[@idref]"
+        xpath_all_ids = "//@id"
+        
+        all_idrefs = instance_doc.xpath(xpath_all_idrefs)
+        all_ids = instance_doc.xpath(xpath_all_ids)
+        
+        for node in all_idrefs:
+            if node.attrib['idref'] not in all_ids:
+                list_unresolved_ids.append(node)
+                
+        return list_unresolved_ids
+                
+    def _check_idref_with_content(self, instance_doc):
+        list_nodes = []
+        xpath = "//*[@idref]"
+        nodes = instance_doc.xpath(xpath)
+        
+        for node in nodes:
+            if node.text or len(node) > 0:
+                list_nodes.append(node)
+                
+        return list_nodes
+    
+    def _check_indicator_practices(self, instance_doc):
+        list_indicators = []
+        xpath = "//%s:Indicator | %s:Indicator" % (self.PREFIX_STIX_CORE, self.PREFIX_STIX_INDICATOR)
+        
+        nodes = instance_doc.xpath(xpath, namespaces=self.NS_MAP)
+        for node in nodes:
+            dict_indicator = {}
+            if not node.attrib.get('idref'): # if this is not an idref node, look at its content
+                if node.find('{%s}Title' % (self.NS_STIX_INDICATOR)) is None:
+                    dict_indicator['title'] = False
+                if node.find('{%s}Description' % (self.NS_STIX_INDICATOR)) is None:
+                    dict_indicator['description'] = False
+                if node.find('{%s}Type' % (self.NS_STIX_INDICATOR)) is None:
+                    dict_indicator['type'] = False
+                if node.find('{%s}Valid_Time_Position' % (self.NS_STIX_INDICATOR)) is None:
+                    dict_indicator['valid_time_position'] = False
+                if node.find('{%s}Indicated_TTP' % (self.NS_STIX_INDICATOR)) is None:
+                    dict_indicator['indicated_ttp'] = False
+                if node.find('{%s}Confidence' % (self.NS_STIX_INDICATOR)) is None:
+                    dict_indicator['confidence'] = False
+                
+                if dict_indicator:
+                    dict_indicator['id'] = node.attrib.get('id')
+                    dict_indicator['node'] = node
+                    list_indicators.append(dict_indicator)
+                
+        return list_indicators
+ 
+    def check_best_practices(self, instance_doc):
+        instance_doc.seek(0)
+        tree = et.parse(instance_doc)
+        root = tree.getroot()
+        
+        list_unresolved_idrefs = self._check_idref_resolution(root)
+        dict_duplicate_ids = self._check_duplicate_ids(root)
+        dict_presence_and_format = self._check_id_presence_and_format(root)
+        list_idref_with_content = self._check_idref_with_content(root)
+        list_indicators = self._check_indicator_practices(root)
+        
+        return {'unresolved_idrefs' : list_unresolved_idrefs,
+                'duplicate_ids' : dict_duplicate_ids,
+                'missing_ids' : dict_presence_and_format['no_id'],
+                'id_format' : dict_presence_and_format['format'],
+                'idref_with_content' : list_idref_with_content,
+                'indicator_suggestions' : list_indicators }
+    
+    def validate(self, instance_doc):
+        '''Validates a STIX document and checks best practice guidance if STIXValidator
+        was initialized with best_practices=True.
+        
+        Best practices will not be checked if the document is schema-invalid.
+        
+        Returns a tuple of (bool, str, dict) for (is valid, validation error, best practice suggestions).
+        
+        Keyword Arguments
+        instance_doc - a file-like object for a STIX instance document
+        '''
+        (isvalid, validation_error) = super(STIXValidator, self).validate(instance_doc)
+        
+        if self.best_practices and isvalid:
+            best_practice_warnings = self.check_best_practices(instance_doc)
+        else:
+            best_practice_warnings = None
+            
+        return (isvalid, validation_error, best_practice_warnings)
+        
  
